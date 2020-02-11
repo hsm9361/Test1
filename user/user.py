3b4b77c5ed83530c036524d8f0791213bf3a09fd
홍사명 <ghdtkaud@gmail.com>
2020-02-11 09:36:12 +0900

    메인화면추가 및 board수정

---
+++ user/user.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+user_blue = Blueprint('user_blue', __name__)
+
+@user_blue.route('/log_in')
+def log_in():
+    html = render_template('user/log_in.html')
+    return html
+
+@user_blue.route('/mypage')
+def mypage():
+    html = render_template('user/mypage.html')
+    return html
