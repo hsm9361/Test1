ba8563bca1f38e836d5d6500c14435f8d08c6262
sooori95 <sooori95@gmail.com>
2020-02-11 17:52:41 +0900

    게시판목록 게시글작성 수정 상세보기

--- board/board.py
+++ board/board.py
@@ -2,8 +2,26 @@ from flask import Blueprint, render_template
 
 board_blue = Blueprint('board_blue', __name__)
 
-# 게시판목록
+# 게시판 목록
 @board_blue.route('/board_list')
 def board_list():
     html = render_template('board/board_list.html')
+    return html
+
+# 게시글 보기
+@board_blue.route('/board_read')
+def board_read():
+    html = render_template('board/board_read.html')
+    return html
+
+# 게시글 쓰기
+@board_blue.route('/board_write')
+def board_write():
+    html = render_template('board/board_write.html')
+    return html
+
+# 게시글 수정
+@board_blue.route('/board_modify')
+def board_modify():
+    html = render_template('board/board_modify.html')
     return html
